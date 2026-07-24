47846a5f4dd9f7748e1ade91efad5eefb25532c6
megha-pnp <meghakathuriaofficial@gmail.com>
2026-07-24 19:17:04 +0530

    Variables

---
+++ variable.js
@@ -0,0 +1,32 @@
+// Variable -> Assignment 1
+// section A
+// let fullName = "Megha ji";
+// let Age = 20;
+// let currentSem = 5;
+// let hasLaptop = true;
+// let AverageAttendance = "85%";
+// console.log(fullName,"\n",Age,"\n",currentSem,"\n",hasLaptop,"\n",AverageAttendance)
+
+// let city = "Delhi";
+// let graduationYear = "2027";
+// let isHosteller = true;
+// let cgpa = 8.5;
+// let student = {course: "BSc", collegeName: "Govt College"};
+//  console.log(student);
+
+// let firstName = "Arjun";
+// let lastName = "Reddy";
+// let swap = firstName;
+// firstName = lastName;
+// lastName = swap;
+// console.log(firstName,"\n",lastName);
+
+// section 2
+// part A
+// console.log("5" + 10);
+// console.log("10" * "2");
+
+// part B
+console.log(Number("50"));
+console.log(String(123));
+console.log(Boolean("non-empty string"));
